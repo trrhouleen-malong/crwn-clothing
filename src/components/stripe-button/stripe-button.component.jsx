b69ad81e291a125e317679cd2c9849928b41f241
Rhouleen Malong <rhouleenmalong@TRAdmins-iMac.local>
2021-07-06 10:17:28 +0800

    updating stripe integration and updating our favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey = 'pk_test_51JA2ZLEFpjvAR0sxSS5ct9jpLUdUMi5SdoYpQuwv474W6JGrPdJdRGFKkFMcvwM2vtk2fYJXZg3EonIeDQawjw6p00Lb8XTjQg';
+
+  const onToken = token => {
+    alert('Payment Successful');
+  }
+
+  return (
+    <StripeCheckout 
+      label='Pay Now'
+      name='CRWN Clothing Ltd.'
+      billingAddress
+      shippingAddress
+      image='https://sendeyo.com/en/f3eb2117da'
+      description={`Your total is $${price}`}
+      amount={priceForStripe}
+      panelLabel='Pay Now'
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  )
+}
+
+export default StripeCheckoutButton;
