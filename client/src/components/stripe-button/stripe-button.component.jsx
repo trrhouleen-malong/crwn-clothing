6de7a8c6e26ee4c7ff1a6cf3b8f1227ff8ca6bcc
Rhouleen Malong <rhouleenmalong@TRAdmins-iMac.local>
2021-07-09 10:01:06 +0800

    removed gitignore files

--- client/src/components/stripe-button/stripe-button.component.jsx
+++ client/src/components/stripe-button/stripe-button.component.jsx
@@ -1,12 +1,27 @@
 import React from 'react';
 import StripeCheckout from 'react-stripe-checkout';
+import axios from 'axios';
 
 const StripeCheckoutButton = ({ price }) => {
   const priceForStripe = price * 100;
   const publishableKey = 'pk_test_51JA2ZLEFpjvAR0sxSS5ct9jpLUdUMi5SdoYpQuwv474W6JGrPdJdRGFKkFMcvwM2vtk2fYJXZg3EonIeDQawjw6p00Lb8XTjQg';
 
   const onToken = token => {
-    alert('Payment Successful');
+    axios({
+      url: 'payment',
+      method: 'post',
+      data: {
+        amount: priceForStripe,
+        token
+      }
+    }).then(response => {
+      alert('Payment successful!');
+    }).catch(error => {
+      console.log('Payment error: ', error);
+      alert(
+        'There was an issue with your payment. Please make sure you use the provided credit card.'
+      );
+    })
   }
 
   return (
